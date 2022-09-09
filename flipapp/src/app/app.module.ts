441aa6dd3e83c0609331764a8acc7f926c855ea1
aakashdeveloper <ahanda205@gmail.com>
2022-09-09 08:54:21 +0530

    owlcrousel

--- flipapp/src/app/app.module.ts
+++ flipapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Footer/footer.component';
@@ -25,7 +26,8 @@ import { MyUpperPipe } from './pipes/myupper.pipe';
 
   // All the modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule
   ],
 
   // All the services
--- flipapp/src/app/models/home.model.ts
+++ flipapp/src/app/models/home.model.ts
@@ -4,3 +4,41 @@ export interface ITop{
     name: string,
     imgUrl:string
 }
+
+
+export interface ICloths{
+    _id: string,
+    image: string,
+    brand: string,
+    description: string,
+    new_price: number,
+    old_price: number,
+    discount: number,
+    delivery_type: string,
+    hidden_stars: number,
+    item_id:number
+}
+
+
+export interface IMobile{
+    _id: string,
+    image: string,
+    description: string,
+    stars: number,
+    ratings: string ,
+    reviews:  string,
+    more_data: string,
+    more_data2: string,
+    more_data3: string,
+    more_data4: string,
+    more_data5: string,
+    more_data6: string,
+    new_price: number,
+    old_price: number,
+    discount: number,
+    delivery_type: string,
+    offer: string,
+    offer2:  string,
+    hidden_stars: number,
+    item_id:number
+}
