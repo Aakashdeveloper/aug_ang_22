3044144940c3d3df1089442c6fa345d7d7dae0fa
aakashdeveloper <ahanda205@gmail.com>
2022-09-27 08:55:14 +0530

    validation

--- templateform/src/app/Forms/form.component.ts
+++ templateform/src/app/Forms/form.component.ts
@@ -8,6 +8,25 @@ import { NgForm } from '@angular/forms';
 
 export class FormsComponent{
 
-    myEmployee = new IUser('Amit','','','','')
-    
+    myEmployee = new IUser('','','','','')
+
+    firstToUpper(value:string):void{
+        if(value.length>0){
+            value = value.trim();
+            this.myEmployee.firstName = value.charAt(0).toUpperCase()+value.slice(1).toLowerCase()
+        }else{
+            this.myEmployee.firstName = value;
+        }
+    }
+
+    showPassword(event:Event):void{
+        console.log((event.target as HTMLInputElement).type)
+        let inputType = (event.target as HTMLInputElement).type;
+        if(inputType === 'password'){
+            (event.target as HTMLInputElement).type = 'text'
+        }else{
+            (event.target as HTMLInputElement).type = 'password'
+        }
+    }
+
 }
